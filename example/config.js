066ab0fcd01c52468ab4deacf79ca3e04cca692b
zephyr <zhangxin@quancheng-ec.com>
2018-02-28 14:05:41 +0800

    update

--- example/config.js
+++ example/config.js
@@ -7,9 +7,16 @@ module.exports = {
   saluki2: {
     name: pkg.name,
     version: pkg.version,
-    urls: 'http://10.42.169.144:8761/eureka/apps,http://10.42.10.250:8761/eureka/apps,http://10.42.140.37:8761/eureka/apps',
+    urls:
+      'http://10.42.169.144:8761/eureka/apps,http://10.42.10.250:8761/eureka/apps,http://10.42.140.37:8761/eureka/apps',
     services: {
-      UserService: 'com.quancheng.zeus.service.UserService:zeus-service-fk:1.0.0'
+      UserService:
+        'com.quancheng.zeus.service.UserService:zeus-service-fk:1.0.0',
+      SmsService: {
+        package:
+          'com.quancheng.samoyed.sms.service.SmsService:samoyed-service:1.0.0',
+        source: 'node_modules/@quancheng'
+      }
     }
   }
 }
